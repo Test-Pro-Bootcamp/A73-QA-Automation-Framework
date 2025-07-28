f64092efe243bd90d3e621acf0807d36da4a8d6c
carinabugho001 <carina@testpro.io>
2025-07-28 17:40:54 +0800

    Added some test sample
    Added some test sample1

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,4 +1,6 @@
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
@@ -7,19 +9,65 @@ import org.testng.annotations.Test;
 import java.time.Duration;
 
 public class LoginTests extends BaseTest {
+
+    public String url = "https://qa.koel.app/";
+
     @Test
     public void loginEmptyEmailPassword() {
 
-//      Added ChromeOptions argument below to fix websocket error
         ChromeOptions options = new ChromeOptions();
         options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 
+
         String url = "https://qa.koel.app/";
         driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
+
+    @Test
+    public void loginInvalidEmailPassword() throws InterruptedException {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        WebDriver driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        navigateURl(url);
+
+        //Step 1 opening koel app
+        String koelURL = "https://qa.koel.app/";
+        driver.get(koelURL);
+
+        //Step 2 enter valid email
+        WebElement emailField = driver.findElement(By.xpath("//input[@type='email']"));
+        emailField.clear();
+        emailField.sendKeys("tes123@testpro.io");
+        Thread.sleep(2000);
+
+        //Step 3 enter valid password
+        WebElement passwordField = driver.findElement(By.xpath("//input[@type='password']"));
+        passwordField.clear();
+        passwordField.sendKeys("qatester");
+        Thread.sleep(2000);
+
+        //Step 4 click login button
+        WebElement loginButton = driver.findElement(By.xpath("//button[@type='submit']"));
+        loginButton.click();
+        Thread.sleep(2000);
+
+        //Step 5 Validation of expected result
+      //  WebElement avatar = driver.findElement(By.xpath("//img[@class='avatar']"));
+
+       Assert.assertEquals(driver.getCurrentUrl(), url );
+
+        driver.quit();
+
+
+    }
+
+
 }
