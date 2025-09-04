9dfec0d7ae10776591c91d5c098479f868045318
simplyjuless <julia.munoz@testpro.io>
2025-09-04 18:53:56 -0500

    Fix Homework 17: driver initialization, removed thread sleeps, added WebDriverWait

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -6,12 +6,29 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.BeforeSuite;
 
+import java.time.Duration;
+
 public class BaseTest {
+   protected WebDriver driver;
+   public String url = "https://qa.koel.app/";
 
     @BeforeSuite
     static void setupClass() {
-
         WebDriverManager.chromedriver().setup();
     }
 
+    @BeforeMethod
+    public void launchBrowser() {
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--remote-allow-origins=*");
+
+        driver = new ChromeDriver(options);
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+        driver.manage().window().maximize();
+
+    }
+    @AfterMethod
+    public void closerBrowser() {
+        driver.quit();
+    }
 }
