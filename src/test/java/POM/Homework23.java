9f06f8a047af22347dd9cefba2846cc2b9dfcd0a
simplyjuless <julia.munoz@testpro.io>
2025-09-17 14:16:16 -0500

    Update BaseTest with SeleniumGrid Browser Factory

--- src/test/java/POM/Homework23.java
+++ src/test/java/POM/Homework23.java
@@ -1,18 +1,7 @@
 package POM;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.Keys;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.Assert;
 import org.testng.annotations.Test;
-import org.testng.internal.BaseTestMethod;
-
-
-import java.time.Duration;
 
 public class Homework23 extends BaseTest {
     //public static Actions actions = null;
